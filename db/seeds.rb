d507249380baae1539b5e655f498a4e094ecbffb
Abiyasu Zhang <abiyasuzhang@gmail.com>
2023-08-05 16:22:04 +0900

    work

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Doc.destroy_all
+
+user1 = User.create!(first_name: "Valerie", last_name: "Keeton", user_name: "Valerie", job_title: "UX Lead", email: "valrie104@gmail.com", password: "123456")
+
+user2 = User.create!(first_name: "Luo", last_name: "Zhen", user_name: "Buruburu", job_title: "Project Manager", email: "zhenluo1988@gmail.com", password: "123456")
+
+user3 = User.create!(first_name: "Aaron", last_name: "Randal", user_name: "Aaron", job_title: "Dev Lead", email: "aaron@gmail.com", password: "123456")
+
+user4 = User.create!(first_name: "Abiyasu", last_name: "Zhang", user_name: "Abiyasu", job_title: "Support Lead", email: "zhangabiyasu@gmail.com", password: "123456")
