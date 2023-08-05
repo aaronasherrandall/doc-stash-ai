e2261eb9396bd61d583ed3332321025d08cafbfa
Aaron Randall <aaronasherrandall@gmail.com>
2023-08-05 14:31:40 +0900

    updated models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :docs, :chats
+  validates :first_name, :last_name, :user_name, :job_title, presence: true
+  validates :user_name, uniqueness: true
 end
